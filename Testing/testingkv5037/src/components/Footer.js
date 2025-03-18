a1264e0ac557397eca21b87c9870df5b9e0ca2b3
W23021055 <w23021055@northumbria.ac.uk>
2025-03-18 14:44:36 +0000

    Update

--- Testing/testingkv5037/src/components/Footer.js
+++ Testing/testingkv5037/src/components/Footer.js
@@ -2,17 +2,33 @@ import React from 'react';
 import '../App.css';
 
 const Footer = () => {
+  
+  // Subheadings for each option
+  const subheadings = {
+    'Privacy policy': ['Overview', 'Data Collection', 'User Rights'],
+    'Terms of service': ['General Terms', 'Usage Policy', 'Liability'],
+    'Contact us': ['Support', 'Feedback', 'Sales Inquiry'],
+  };
+
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-links">
-        {['Privacy policy', 'Terms of service', 'Contact us'].map((option) => (
-            <a
-              key={option}
-              href={option.replace(/\s/g, '-')}
-            >
-              {option}
-            </a>
+        <div className="footer-text">
+          {['Privacy policy', 'Terms of service', 'Contact us'].map((option) => (
+            <div key={option}>
+              <a href={option.replace(/\s/g, '-')}>
+                {option}
+              </a>
+              <br />
+              {subheadings[option].map((subheading) => (
+                <div key={subheading}>
+                  <a href={subheading.replace(/\s/g, '-')}>
+                    {subheading}
+                  </a>
+                  <br />
+                </div>
+              ))}
+            </div>
           ))}
         </div>
       </div>
