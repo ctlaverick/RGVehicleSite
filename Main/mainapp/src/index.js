e9038b76c41fff47ea8097636ef1d6a0af698118
Wilbur15 <williamoliveruk@gmail.com>
2025-04-01 19:52:44 +0100

    Main Update

--- Main/mainapp/src/index.js
+++ Main/mainapp/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <div className='bg-Background min-h-full min-w-full'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/About-Us' element={<About />} />
+          <Route path='/Services' element={<Services />} />
+          <Route path='/Contact' element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ Main/mainapp/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CarOption from '../components/CarOption'
+
+const Home = () => {
+  return (
+    <div className='p-8 text-center'>
+      <h1 className='text-3xl font-bold'>Home!</h1>
+      <CarOption />
+    </div>
+  );
+};
+
+export default Home;
