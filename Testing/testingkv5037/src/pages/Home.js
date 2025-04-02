e3d815d02ca8d12e502d83b04c16bc7c89bc3eab
W23021055 <w23021055@northumbria.ac.uk>
2025-04-02 16:02:34 +0100

    Updated

--- Testing/testingkv5037/src/pages/Home.js
+++ Testing/testingkv5037/src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import CarOption from '../components/CarOption'
+import CarOption from '../components/CarOption';
+
+
+const images = require.context('../images/cars', false, /\.(jpg|jpeg|png)$/);
+const carImages = images.keys().map(image => ({
+  name: image.replace('./', '').replace(/\.(jpg|jpeg|png)$/, ''),
+  image: images(image),
+}));
 
 const Home = () => {
   return (
     <div className='p-8 text-center'>
-      <h1 className='text-3xl font-bold'>Home!</h1>
-      <CarOption />
+      <h1 className="text-xl font-semibold">Recommended cars:</h1>
+      <div className="flex flex-wrap justify-center gap-4">
+        {carImages.map((car, index) => (
+          <CarOption key={index} name={car.name} image={car.image} />
+        ))}
+      </div>
     </div>
   );
 };
