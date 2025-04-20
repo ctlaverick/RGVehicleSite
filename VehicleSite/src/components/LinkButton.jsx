6bd9aaae2133be8fb8bd118daa2ef604efcaec57
ctlaverick <ctlaverick3@gmail.com>
2025-04-20 14:57:53 +0100

    Vehicles Link to Overview

--- VehicleSite/src/components/LinkButton.jsx
+++ VehicleSite/src/components/LinkButton.jsx
@@ -1,7 +1,7 @@
 const LinkButton = ({ text, link, className }) => {
     return (
       <a 
-      className={`bg-success text-admin-text py-2 px-4 rounded-lg hover:bg-green-600 hover:text-secondary cursor-pointer mt-2 ${className}`}
+      className={`${className} ? ${className} : bg-success text-admin-text py-2 px-4 rounded-lg hover:bg-green-600 hover:text-secondary cursor-pointer mt-2`}
       href={link}>
         {text}
       </a>
